e033155c9fab2039ae04fdd080df55e220b4c988
Perry Naseck <DaAwesomeP@users.noreply.github.com>
2021-06-28 10:31:28 -0400

    make into Python package

---
+++ arenarobot/__init__.py
@@ -0,0 +1,24 @@
+"""
+__init__.py
+Created by Perry Naseck on 6/28/21.
+
+Copyright (c) 2021, The CONIX Research Center
+All rights reserved.
+
+This source code is licensed under the BSD-3-Clause license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+try:
+    from _version import version
+except ImportError:
+    try:
+        from setuptools_scm import get_version
+        version = get_version()
+    except (ImportError, LookupError):
+        version = 'dev'
+__version__ = version
+__author__      = "The CONIX Research Center"
+
+__license__     = "BSD 3-Clause"
+__maintainer__  = "WiSE Lab, Carnegie Mellon University"
---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+"""
+setup.py
+Created by Perry Naseck on 6/28/21.
+
+Copyright (c) 2021, The CONIX Research Center
+All rights reserved.
+
+This source code is licensed under the BSD-3-Clause license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+import pathlib
+from setuptools import setup, find_packages
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name="arena-robot",
+    description="Robot integration runtime for the ARENA",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='BSD-3-Clause',
+    author="Conix Research Center",
+    author_email="info@conix.io",
+    python_requires='>=3.6, <4',
+    packages=find_packages(),
+    platforms=["any"],
+    url="https://github.com/conix-center/ARENA-robot",
+    project_urls={
+        'Bug Reports': 'https://github.com/conix-center/ARENA-robot/issues',
+        'Source': 'https://github.com/conix-center/ARENA-robot',
+    },
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering',
+        'Framework :: Robot Framework'
+    ],
+    keywords='arena robot ar vr robotics',
+    install_requires=[
+        'arena-py>=0,<1',
+        'numpy>=1,<2',
+        'setuptools_scm>=6.0'
+    ]
+)
