b7289880e1a80cbb3ff8ac4a0eb86947d4e5df24
Perry Naseck <git@perrynaseck.com>
2022-02-03 15:30:59 -0500

    use new Beluga AT+REBOOT, fix Beluga starting from unknown state

--- arenarobot/service/sensor/beluga_serial.py
+++ arenarobot/service/sensor/beluga_serial.py
@@ -61,6 +61,14 @@ class ArenaRobotServiceSensorBelugaSerial(ArenaRobotServiceSensor):
         self.aioserial_instance = aioserial.AioSerial(port=self.dev_path,
                                                       baudrate=self.baudrate)
 
+        def msg_in_clear():
+            # Wait and clear all of the messages that come in
+            # Python seems bad at clearing the buffers the first time
+            for i in range(2):
+                while self.aioserial_instance.in_waiting > 0:
+                    self.aioserial_instance.reset_input_buffer()
+                time.sleep(1)
+
         async def wait_for_msg(wait_msg: str):
             while True:
                 msg_bytes = await self.aioserial_instance.readline_async()
@@ -68,79 +76,59 @@ class ArenaRobotServiceSensorBelugaSerial(ArenaRobotServiceSensor):
                 if msg.startswith(wait_msg):
                     break
 
-        time.sleep(2)
-        self.aioserial_instance.reset_input_buffer()
-        time.sleep(1)
-
-        # See https://github.com/WiseLabCMU/Beluga/issues/2
-        # print("STOPUWB")
-        # stop_uwb_msg = f"AT+STOPUWB\n".encode()
-        # await self.aioserial_instance.write_async(stop_uwb_msg)
-        # await wait_for_msg("OK")
-        # time.sleep(0.25)
-
-        # See https://github.com/WiseLabCMU/Beluga/issues/2
-        if self.aioserial_instance.in_waiting > 0:
-            stop_ble_msg = "AT+STOPBLE\n".encode()
-            await self.aioserial_instance.write_async(stop_ble_msg)
-            await wait_for_msg("OK")
-            time.sleep(0.25)
+        msg_in_clear()
 
         set_id_msg = "AT+RESET\n".encode()
         await self.aioserial_instance.write_async(set_id_msg)
         await wait_for_msg("Reset OK")
-        time.sleep(0.25)
+        time.sleep(1)
+
+        set_id_msg = "AT+REBOOT\n".encode()
+        await self.aioserial_instance.write_async(set_id_msg)
+
+        msg_in_clear()
 
         set_id_msg = f"AT+ID {self.beluga_id}\n".encode()
         await self.aioserial_instance.write_async(set_id_msg)
         await wait_for_msg("OK")
-        time.sleep(0.25)
 
         set_boot_mode_msg = "AT+BOOTMODE 0\n".encode()
         await self.aioserial_instance.write_async(set_boot_mode_msg)
         await wait_for_msg("Bootmode: 0 OK")
-        time.sleep(0.25)
 
         if self.beluga_rate is not None:
             set_rate_msg = f"AT+RATE {self.beluga_rate}\n".encode()
             await self.aioserial_instance.write_async(set_rate_msg)
             await wait_for_msg(f"Rate: {self.beluga_rate} OK")
-            time.sleep(0.25)
 
         if self.beluga_channel is not None:
             set_channel_msg = f"AT+CHANNEL {self.beluga_channel}\n".encode()
             await self.aioserial_instance.write_async(set_channel_msg)
             await wait_for_msg("OK")
-            time.sleep(0.25)
 
         if self.beluga_tx_power is not None:
             set_tx_power_msg = f"AT+TXPOWER {self.beluga_tx_power}\n".encode()
             await self.aioserial_instance.write_async(set_tx_power_msg)
             await wait_for_msg("OK")
-            time.sleep(0.25)
 
         if self.beluga_timeout is not None:
             set_timeout_msg = f"AT+TIMEOUT {self.beluga_timeout}\n".encode()
             await self.aioserial_instance.write_async(set_timeout_msg)
             await wait_for_msg("OK")
-            time.sleep(0.25)
 
         set_stream_mode_msg = "AT+STREAMMODE 0\n".encode()
         await self.aioserial_instance.write_async(set_stream_mode_msg)
         await wait_for_msg("OK")
-        time.sleep(0.25)
 
         if self.beluga_twr_mode is not None:
             set_twr_mode_msg = f"AT+TWRMODE {self.beluga_twr_mode}\n".encode()
             await self.aioserial_instance.write_async(set_twr_mode_msg)
             await wait_for_msg("OK")
-            time.sleep(0.25)
 
         if self.beluga_led_mode is not None:
             set_led_mode_msg = f"AT+LEDMODE {self.beluga_led_mode}\n".encode()
             await self.aioserial_instance.write_async(set_led_mode_msg)
             await wait_for_msg("OK")
-            time.sleep(0.25)
 
         start_ble_msg = "AT+STARTBLE\n".encode()
         await self.aioserial_instance.write_async(start_ble_msg)
