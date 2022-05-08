8453d90c3322e802366b79cd334004ddce59d0bc
Perry Naseck <git@perrynaseck.com>
2022-05-08 11:34:24 -0400

    fix waypoint msg

--- arenadrone/drone_to_arena_new.py
+++ arenadrone/drone_to_arena_new.py
@@ -454,11 +454,9 @@ def drone_sensor_message(client, userdata, msg):
 device.message_callback_add(CUSTOM_TOPIC_lidar, drone_sensor_message)
 
 def device_target_location_update(client, userdata, msg):
-    payload_str = msg.payload.decode("utf-8", "ignore").replace("\\", "")[1:-1]
+    payload_str = msg.payload.decode("utf-8", "ignore").replace("\\", "")
     payload = json.loads(payload_str)
-    print(payload)
-    waypoint_msg = payload["msg"]["data"][0]
-    pos = waypoint_msg["target"]["position"]
+    pos = payload["target"]["position"]
     # yaw = msg.target.rotation.y
     print("***************************************************")
     # print("DEVICE TARGET UPDATE:", pos, yaw)
