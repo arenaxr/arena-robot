706fdc6981a3a2d9f387c116697544ab7c9e6637
Perry Naseck <git@perrynaseck.com>
2022-07-08 15:18:15 -0400

    clean up t265 mjpeg demo code

--- misc-demo/rs-t265-mjpeg-server.py
+++ misc-demo/rs-t265-mjpeg-server.py
@@ -154,28 +154,3 @@ def main():
 if __name__ == "__main__":
     RES = main()
     exit(RES)
-
-
-def main() -> int:
-    """Run an ARENA-robot service."""
-    parser = argparse.ArgumentParser(description='Run an ARENA-robot service.')
-    parser.add_argument('service_config_file',  type=str,
-                        help='service file device path')
-    args = parser.parse_args()
-
-    with open(args.service_config_file, encoding="utf8") as file:
-        service_config = json.load(file)
-
-    service_type = service_config.get("service_type", "unknown")
-    if "service_type" in service_config:
-        del service_config["service_type"]
-
-    service_class = service.SERVICE_TYPE_MAP[service_type]
-
-    service_instance = service_class(**service_config)
-    if service_instance.async_service:
-        (asyncio.get_event_loop()
-            .run_until_complete(service_instance.async_setup()))
-    else:
-        service_instance.setup()
-    service_instance.start()
